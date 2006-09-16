6eee74845aa3e635ce7e04924f04e65b4d3a6bb2
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2006-09-16 14:37:51 +0000

    tuning. libc is 1-2 seconds faster to load now.
    svn:sync-xref-src-repo-rev: r526

--- src/gmake/kmkbuiltin/mscfakes.h
+++ src/gmake/kmkbuiltin/mscfakes.h
@@ -1,4 +1,4 @@
-/* $Id: mscfakes.h 370 2005-12-18 03:48:02Z knut.osmundsen@oracle.com $ */
+/* $Id: mscfakes.h 526 2006-09-16 14:37:51Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * Unix fakes for MSC.
@@ -119,6 +119,8 @@ int mkstemp(char *temp);
 #define reallocf(old, size) realloc(old, size)
 #if _MSC_VER < 1400
 int snprintf(char *buf, size_t size, const char *fmt, ...);
+#else
+#define snprintf _snprintf
 #endif 
 size_t strlcpy(char *, const char *, size_t);
 int symlink(const char *pszDst, const char *pszLink);
