51b0bb0fef9bbdfaf9dca0dc2b686c92e7d10a7e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2007-06-04 01:28:44 +0000

    some figures with correct kmk multi-target code.
    svn:sync-xref-src-repo-rev: r1024

--- src/kObjCache/kObjCache.c
+++ src/kObjCache/kObjCache.c
@@ -1,4 +1,4 @@
-/* $Id: kObjCache.c 1017 2007-06-03 02:49:52Z knut.osmundsen@oracle.com $ */
+/* $Id: kObjCache.c 1024 2007-06-04 01:28:44Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * kObjCache - Object Cache.
@@ -1707,7 +1707,7 @@ int main(int argc, char **argv)
             return usage();
         else if (!strcmp(argv[i], "-V") || !strcmp(argv[i], "--version"))
         {
-            printf("kObjCache v0.0.0 ($Revision: 1017 $)\n");
+            printf("kObjCache v0.0.0 ($Revision: 1024 $)\n");
             return 0;
         }
         else
@@ -1758,11 +1758,15 @@ int main(int argc, char **argv)
  *  real    11m42.513s
  *  user    14m27.736s
  *  sys     3m39.512s
- *
- * Mac OS X debug -j 3 cached clobber build (touch include/iprt/err.h ; sync ; svn diff ; sync ; sleep 1 ; time kmk -j 3 USE_KOBJCACHE=1):
- *  real    2m24.712s       real    2m10.909s  
- *  user    2m15.339s       user    2m15.146s  
- *  sys     0m56.278s       sys     0m55.591s  
- *  !Stuff is built three times here because of + instead of +| in footer.kmk!
+ * 
+ * Mac OS X debug -j 3 cached depend build (touch include/iprt/err.h ; sync ; svn diff ; sync ; sleep 1 ; time kmk -j 3 USE_KOBJCACHE=1):
+ *  real    1m17.445s
+ *  user    1m13.410s
+ *  sys     0m22.789s
+ * 
+ * Mac OS X debug -j3 cached depend build (touch include/iprt/cdefs.h ; sync ; svn diff ; sync ; sleep 1 ; time kmk -j 3 USE_KOBJCACHE=1):
+ *  real    1m29.315s
+ *  user    1m31.391s
+ *  sys     0m32.748s
  * 
  */
