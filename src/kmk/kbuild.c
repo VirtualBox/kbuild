d8c17a0da5948207edcb218de886af4fd1a00153
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2008-09-21 01:17:50 +0000

    kmk: minor incdep adjustments.
    svn:sync-xref-src-repo-rev: r1799

--- src/kmk/kbuild.c
+++ src/kmk/kbuild.c
@@ -1,4 +1,4 @@
-/* $Id: kbuild.c 1797 2008-09-21 01:05:36Z knut.osmundsen@oracle.com $ */
+/* $Id: kbuild.c 1799 2008-09-21 01:17:50Z knut.osmundsen@oracle.com $ */
 /** @file
  *
  * kBuild specific make functionality.
@@ -1703,7 +1703,7 @@ func_kbuild_source_one(char *o, char **argv, const char *pszFuncName)
      * argv[0] is the function version. Prior to r1792 (early 0.1.5) this
      * was undefined and footer.kmk always passed an empty string.
      *
-     * Version 2, as implemented in r1796, will make use of the async
+     * Version 2, as implemented in r1797, will make use of the async
      * includedep queue feature. This means the files will be read by one or
      * more background threads, leaving the eval'ing to be done later on by
      * the main thread (in snap_deps).
