aa4c8a49d9d36c94756011121672ca943ea436f8
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2011-07-07 11:59:15 +0000

    again...
    svn:sync-xref-src-repo-rev: r2447

--- src/lib/restartable-syscall-wrappers.c
+++ src/lib/restartable-syscall-wrappers.c
@@ -1,4 +1,4 @@
-/* $Id: restartable-syscall-wrappers.c 2446 2011-07-07 11:58:12Z knut.osmundsen@oracle.com $ */
+/* $Id: restartable-syscall-wrappers.c 2447 2011-07-07 11:59:15Z knut.osmundsen@oracle.com $ */
 /** @file
  * restartable-syscall-wrappers.c - Workaround for annoying S11 "features".
  *
@@ -209,6 +209,7 @@ static int dlsym_libc(const char *pszSymbol, void **ppvSym)
     return 0;
 }
 
+#undef fopen
 FILE *fopen(const char *pszName, const char *pszMode)
 {
     static union
@@ -228,6 +229,7 @@ FILE *fopen(const char *pszName, const char *pszMode)
     return pFile;
 }
 
+#undef fopen64
 FILE *fopen64(const char *pszName, const char *pszMode)
 {
     static union
