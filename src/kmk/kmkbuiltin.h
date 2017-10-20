b4dae44d4df0bf61dc24190745ef11248a72adab
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2017-10-20 17:01:40 +0000

    hurd workaround
    svn:sync-xref-src-repo-rev: r3108

--- src/kmk/kmkbuiltin.h
+++ src/kmk/kmkbuiltin.h
@@ -1,4 +1,4 @@
-/* $Id: kmkbuiltin.h 3107 2017-10-20 16:53:50Z knut.osmundsen@oracle.com $ */
+/* $Id: kmkbuiltin.h 3108 2017-10-20 17:01:40Z knut.osmundsen@oracle.com $ */
 /** @file
  * kMk Builtin command handling.
  */
@@ -35,9 +35,17 @@
 #endif
 
 /* For the GNU/hurd weirdo. */
-#if !defined(PATH_MAX) && !defined(_MAX_PATH)
-# define PATH_MAX 4096
+#ifndef PATH_MAX
+# ifdef MAXPATHLEN
+#  define PATH_MAX  MAXPATHLEN
+# else
+#  define PATH_MAX  4096
+# endif
 #endif
+#ifndef MAXPATHLEN
+# define MAXPATHLEN PATH_MAX
+#endif
+
 
 #include "kbuild_version.h"
 
