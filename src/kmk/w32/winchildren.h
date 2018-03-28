a65b07526f684b9be4557156bea2e0039abc1ed4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2018-03-28 20:32:11 +0000

    kmk,kWorker: Some fixes wrt output capture and ctrl-c.
    svn:sync-xref-src-repo-rev: r3200

--- src/kmk/w32/winchildren.h
+++ src/kmk/w32/winchildren.h
@@ -1,4 +1,4 @@
-/* $Id: winchildren.h 3199 2018-03-28 18:56:21Z knut.osmundsen@oracle.com $ */
+/* $Id: winchildren.h 3200 2018-03-28 20:32:11Z knut.osmundsen@oracle.com $ */
 /** @file
  * Child process creation and management for kmk.
  */
@@ -74,7 +74,8 @@ int     MkWinChildCreateBuiltIn(struct KMKBUILTINENTRY const *pBuiltIn, int cArg
 int     MkWinChildCreateAppend(const char *pszFilename, char **ppszAppend, size_t cbAppend, int fTruncate,
                                struct child *pMkChild, pid_t *pPid);
 
-int     MkWinChildCreateSubmit(intptr_t hEvent, void *pvSubmitWorker, PWINCCWPIPE pStdOut, PWINCCWPIPE pStdErr, pid_t *pPid);
+int     MkWinChildCreateSubmit(intptr_t hEvent, void *pvSubmitWorker, PWINCCWPIPE pStdOut, PWINCCWPIPE pStdErr,
+                               struct child *pMkChild, pid_t *pPid);
 PWINCCWPIPE MkWinChildcareCreateWorkerPipe(unsigned iWhich, unsigned int idxWorker);
 void    MkWinChildcareWorkerDrainPipes(struct WINCHILD *pChild, PWINCCWPIPE pStdOut, PWINCCWPIPE pStdErr);
 void    MkWinChildcareDeleteWorkerPipe(PWINCCWPIPE pPipe);
